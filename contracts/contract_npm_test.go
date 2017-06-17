db76d22d80efe08424fbd01265e0b7bb9805b235
Vishnu Bharathi <vishnubharathi04@gmail.com>
2017-06-17 12:20:51 +0530

    standardizes npm

--- contracts/contract_npm_test.go
+++ contracts/contract_npm_test.go
@@ -4,17 +4,17 @@ import "testing"
 
 func TestBadges(t *testing.T) {
 	testMap := map[string]bool{
-		"[![npm weekly downloads](https://img.shields.io/npm/dw/vici.svg)](https://npmjs.com/package/vici)":         false,
-		"[![npm monthly downloads](https://img.shields.io/npm/dm/vici.svg)](https://npmjs.com/package/vici)":        false,
-		"[![npm yearly downloads](https://img.shields.io/npm/dy/vici.svg)](https://npmjs.com/package/vici)":         false,
-		"[![npm total downloads](https://img.shields.io/npm/dy/vici.svg)](https://npmjs.com/package/vici)":          false,
-		"[![npm version](https://img.shields.io/npm/v/vici.svg)](https://npmjs.com/package/vici)":                   false,
-		"[![npm next version](https://img.shields.io/npm/v/vici/next.svg)](https://npmjs.com/package/vici)":         false,
-		"[![npm canary version](https://img.shields.io/npm/v/vici/canary.svg)](https://npmjs.com/package/vici)":     false,
-		"[![license badge](https://img.shields.io/npm/l/vici.svg)](https://npmjs.com/package/vici)":                 false,
-		"[![snyk - known vulnerabilities](https://snyk.io/test/npm/vici/badge.svg)](https://snyk.io/test/npm/vici)": false,
+		"[![npm weekly downloads](https://img.shields.io/npm/dw/express.svg)](https://npmjs.com/package/express)":         false,
+		"[![npm monthly downloads](https://img.shields.io/npm/dm/express.svg)](https://npmjs.com/package/express)":        false,
+		"[![npm yearly downloads](https://img.shields.io/npm/dy/express.svg)](https://npmjs.com/package/express)":         false,
+		"[![npm total downloads](https://img.shields.io/npm/dy/express.svg)](https://npmjs.com/package/express)":          false,
+		"[![npm version](https://img.shields.io/npm/v/express.svg)](https://npmjs.com/package/express)":                   false,
+		"[![npm next version](https://img.shields.io/npm/v/express/next.svg)](https://npmjs.com/package/express)":         false,
+		"[![npm canary version](https://img.shields.io/npm/v/express/canary.svg)](https://npmjs.com/package/express)":     false,
+		"[![license badge](https://img.shields.io/npm/l/express.svg)](https://npmjs.com/package/express)":                 false,
+		"[![snyk - known vulnerabilities](https://snyk.io/test/npm/express/badge.svg)](https://snyk.io/test/npm/express)": false,
 	}
-	contract := NewNpmBadgeContract("../samples/npm/basic")
+	contract := NewNpmBadgeContract("../samples/npm")
 	badges, err := contract.Badges()
 	if err != nil {
 		t.Fatal("Expected err to be nil, but got", err)
