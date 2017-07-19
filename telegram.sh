9a697f9bca6f095ea7ef30a7992028840c101022
Vishnu Bharathi <vishnubharathi04@gmail.com>
2017-07-20 03:51:44 +0530

    last try for smileys

--- telegram.sh
+++ telegram.sh
@@ -2,5 +2,6 @@
 set -e
 echo "Posting message to Telegram"
 echo $1
+echo "{ \"text\": \"$1\" }"
 echo "---------------------------"
 curl -H "Content-Type: application/json" -X POST -d "{ \"text\": \"$1\" }" $TELEGRAM_HORN
