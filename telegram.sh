6edacefd8ab1cf7c57d38c823e466e892bced93d
Vishnu Bharathi <vishnubharathi04@gmail.com>
2017-07-20 03:21:32 +0530

    edits telegram script

--- telegram.sh
+++ telegram.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 set -e
+echo "Posting message to Telegram"
+echo $1
+echo "---------------------------"
 curl -H "Content-Type: application/json" -X POST -d "{ \"text\": \"$1\" }" $TELEGRAM_HORN
