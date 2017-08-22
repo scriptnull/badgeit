2b83c9677fe3fd98e8d0e444925c3ccf4f07ad67
Vishnu Bharathi <vishnubharathi04@gmail.com>
2017-08-22 19:11:54 +0530

    omits notification for PRs

--- telegram.sh
+++ telegram.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-set -e
-echo "Posting message to Telegram"
-echo $1
-echo "---------------------------"
-curl -H "Content-Type: application/json" -X POST -d "{ \"text\": \"$1\" }" $TELEGRAM_HORN
+
+if [ "$IS_PULL_REQUEST" == false ]
+then
+  set -e
+  echo "Posting message to Telegram"
+  echo $1
+  echo "---------------------------"
+  curl -H "Content-Type: application/json" -X POST -d "{ \"text\": \"$1\" }" $TELEGRAM_HORN
+fi
