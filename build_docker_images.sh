7b443a95c562de5eb9c1380a9bc7fe29d4ce1662
Vishnu Bharathi <vishnubharathi04@gmail.com>
2017-09-02 00:31:57 +0530

    adds func for build worker

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -10,6 +10,14 @@ fi
 API_CHANGED_FILE_COUNT=`git diff --name-only HEAD~1..HEAD api/ | wc -l`;
 WORKER_CHANGED_FILE_COUNT=`git diff --name-only HEAD~1..HEAD worker/ contracts/ common/ formatters/ | wc -l`;
 
+build_worker() {
+    # Build binary and worker
+    docker build -t "scriptnull/badgeit-worker:$BRANCH.$BUILD_NUMBER" .
+    docker images
+    docker push scriptnull/badgeit-worker:$BRANCH.$BUILD_NUMBER
+    ./telegram.sh "New Worker image available: scriptnull/badgeit-worker:$BRANCH.$BUILD_NUMBER"
+}
+
 if [ $API_CHANGED_FILE_COUNT -gt 0 ]; then
     echo "API has changes"
     # Build API
@@ -19,13 +27,14 @@ if [ $API_CHANGED_FILE_COUNT -gt 0 ]; then
     ./telegram.sh "New API image available: scriptnull/badgeit-api:$BRANCH.$BUILD_NUMBER"
 fi
 
-if [ $WORKER_CHANGED_FILE_COUNT -gt 0 || $FORCE_WORKER_BUILD == true ]; then
+if [ $WORKER_CHANGED_FILE_COUNT -gt 0 ]; then
     echo "Worker has changes"
-    # Build binary and worker
-    docker build -t "scriptnull/badgeit-worker:$BRANCH.$BUILD_NUMBER" .
-    docker images
-    docker push scriptnull/badgeit-worker:$BRANCH.$BUILD_NUMBER
-    ./telegram.sh "New Worker image available: scriptnull/badgeit-worker:$BRANCH.$BUILD_NUMBER"
+    build_worker
+fi
+
+if [ $FORCE_WORKER_BUILD == true ]; then
+    echo "FORCE_WORKER_BUILD: $FORCE_WORKER_BUILD"
+    build_worker
 fi
 
 echo "Build Image script End"
